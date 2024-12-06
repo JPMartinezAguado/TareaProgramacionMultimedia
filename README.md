79eb5952a814fb846596a0e0b15ffa8516941a80
JPMartinezAguado <jose.placido.martinez.aguado@fpfomento.com>
2024-12-06 17:26:37 +0100

    Versión incial para tarea 1

--- README.md
+++ README.md
@@ -1,1 +1,94 @@
-"# TareaProgramacionMultimedia" 
+"# TareaProgramacionMultimedia"  
+
+*********************** #uR_Bit. *************************************
+
+Aplicación para la tarea de la asignatura de Programacion Multimedia.
+
+
+**************** DESCRIPCION DE LA APLICACIÓN ************************
+
+La aplicación a desarrollar durante el módulo se trata de una especie
+de tablón de anuncios donde organizaciones de ayuda social comunican
+las acciones concretas que necesitan realizar (de muy diversos tipos, 
+desde simplemente acompañar a una persona anciana a realizar la compra
+hasta la reforma de un centro de acogida, o asistir a un comedor social).
+
+De otra parte, gente dispuesta a colaborar pero que no están integradas
+en dichas organizaciones pueden buscar de forma convniente y efectiva
+las que se ajusten a sus habilidades (cuidado de mayores, juegoterapia,
+comedores sociales, atencion inmigrantes, trabajos domesticos…)
+y disponibilidad, tanto horaria como geográfica.
+
+El nombre elegido para la app ( #uR_Bit. ) es slang de "your bit", que
+hace referencia a una expression inglesa ("to do your bit") que equivale
+a "poner tu granito de arena". El uso de símbolos (# _ .) es solo para
+darle un toque mas contemporáneo, que parece que eso les gusta a los 
+modernos :P. 
+El epígrafe "a place to do your best" pretende reforzar esa idea de 
+accion positiva.
+
+Para describir las funcionalidades que incorporará la aplicacion, a partir
+ de ahora, nos referiremos como ACF (App Completa y Funcional) a lo 
+que supone que realizaría la aplicación si se llevase a termino respecto
+a su conceptualización.
+
+ Llamaremos FTM (Funcionalidades de la Tarea
+del Modulo) a las partes que, en principio y de acuerdo con lo hablado
+contigo al respecto, presupongo que tendrá incorporadas de modo funcional
+al final del módulo de la asignatura.
+
+Con esta distinction hecha, paso a enumerar las diversas funcionalidades
+requeridas para la tarea:
+
+-La app contendrá una interfaz que permitirá visualizar anuncios e interactuar
+con ellos, expandiendolos para ver mas detalles, pudiendo guardarlos en una
+ lista de favoritos. Parte de dicha interfaz no será operativa en la FTM,
+como puede ser el login o las pantallas de creacion de anuncios
+
+-Se conectará a Internet para obtener servicios de ubicación a través de
+Google Maps. Estos datos se usarán para 
+	-En el momento que un voluntario busca anuncios, poder cribar 
+	aquellos que entran dentro del ambito geografico deseado.
+	-Cuando una organizacion publique un anuncio, se le notificará
+	a los voluntarios registrados cuyo rango geografico definido
+	(junto con otros parametros como skills) coincida con la del 
+	anuncio
+
+
+-Manejará una base de datos: 
+	-En la FTM, ésta será de caracter local, con datos "dummy" para 
+	comprobar la funcionalidad de la aplicacion.
+ 	-En la version ACF,dicha base de datos se guardaría en servidores 
+	y se rellenaria a traves de formularios de registro y publicacion de anuncios)
+
+-Contendrá un buscador por el que se pueden restringir los anuncios mostrados 
+en base a distintas variables, como pueden ser:
+	-Ubicacion geografica y distancia con el que realiza la busqueda
+	-Tags de skills/preferencias
+	-Temporalidad
+	-nombre de la ONG...
+
+-Los usuarios podrán guardar en favoritos los anuncios que consideren interesantes
+para ellos, o enviarlos a personas que consideren puedan estar interesados
+
+
+
+****************CONCEPTUALIZACION DE LA ACF********************************************
+
+-Respecto a la ACF, lo ideal para que fuese un proyecto viable seria la incorporación de 
+capital privado. Esto se podria hacer mediante el patrocinio de empresas cuyo
+target de publico coincida con el perfil de usuario que podria tener esta app,
+y, a cambio de visibilidad de sus productos en la app, mediante por ejemplo pequeños 
+banners entre anuncios,  a traves de un programa de puntos o freevies, recompensarian a
+los voluntarios por las acciones que realizaran. Estos podrían tener la opcion de donar 
+dichos rewards a obras sociales.
+
+-Tambien  sería interesante el restringir los contenidos a acciones puramente sociales 
+y de apoyo mutuo, filtrando aquellas con caracter politico, religioso o discriminatorio.
+
+-El ámbito geográfico ideal es el local, acciones a realizar pequeñas y concretas, de duracion
+corta para que aquellas personas que les gustaria ayudar, sientan que aunque su dia a dia
+les restringe la capacidad de accion, puedan encontrar ese rato en la semana en que 
+sentirse utiles.
+
+
---
+++ app/src/main/java/com/jpmartineza/tareaprogramacionmultimedia/MainActivity.kt
@@ -0,0 +1,364 @@
+package com.jpmartineza.tareaprogramacionmultimedia
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.AbsoluteAlignment
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MaquetacionUI()
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Preview
+@Composable
+
+fun MaquetacionUI() {
+
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = { NavegadorLateral(Modifier.width(Dp(200f))) },
+        content = {
+            Scaffold(
+                topBar = { ToolBar(onMenuClick = {
+                    scope.launch {
+                        if (drawerState.isOpen) {
+                            drawerState.close()
+                        } else {
+                            drawerState.open()
+                        }
+                    }
+                })
+                         },
+                //content = {
+                   // innerPadding -> Box(modifier = Modifier.padding(innerPadding)) {
+                content = {Cuerpo()},
+                bottomBar = { BarraInferior() }
+
+            )
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ToolBar(onMenuClick: () -> Unit) {
+    CenterAlignedTopAppBar(
+        navigationIcon = {
+            IconButton(onClick = onMenuClick) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = "Menu",
+                    tint = Color.White
+                )
+            }
+        },
+        title = {
+            Text(
+                text = "#uR_Bit.",
+                color = Color.White,
+                style = MaterialTheme.typography.headlineLarge,
+                fontFamily = cooperFontFamily
+            )
+        },
+        actions =
+        {
+            IconButton(onClick = {}){
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = "Account",
+                    tint = Color.White
+                )
+            }
+
+
+        },
+        colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.DarkGray),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(0.dp, 0.dp, 0.dp, 10.dp)
+    )
+}
+
+@Preview
+@Composable
+fun Cuerpo() {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(30.dp, 40.dp, 30.dp, 5.dp)
+    ) {
+        item {
+            Spacer(modifier = Modifier.height(60.dp))
+            repeat(14) { // Para evitar repetición manual
+                MuestraAnuncio()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun  BarraInferior() {
+    BottomAppBar(
+        containerColor = Color.DarkGray,
+        contentColor = Color.White,
+        modifier = Modifier.height(100.dp),
+        content = {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Refresh,
+                    contentDescription = "email",
+                    tint = Color.White
+                )
+                Icon(
+                    imageVector = Icons.Default.Home,
+                    contentDescription = "Casa",
+                    tint = Color.White
+                )
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "anadir",
+                    tint = Color.White
+                )
+
+
+            }
+
+        }
+    )
+
+
+}
+
+@Composable
+fun NavegadorLateral(modifier: Modifier = Modifier)
+{
+
+    Column(
+        modifier = Modifier
+            .padding(0.dp)
+            .background(Color.White, RectangleShape)
+    ){
+        Box(
+            modifier = Modifier
+                .width(250.dp)
+                .padding(top = 50.dp, bottom = 10.dp)
+                .clip(RoundedCornerShape(9.dp))
+                .padding(0.dp)
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.brand_logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(3.dp)
+                    .align(Alignment.Center)
+            )
+        }
+        ElementosNavLat(icono = Icons.Default.Search, nombre = "Buscador")
+        ElementosNavLat(icono = Icons.Default.Favorite, nombre = "Favoritos")
+        ElementosNavLat(icono = Icons.Default.Create, nombre = "Crear")
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        Box(
+            modifier = Modifier
+                .width(70.dp)
+                .clip(RoundedCornerShape(9.dp))
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(3.dp)
+                    .align(AbsoluteAlignment.TopLeft)
+            )
+        }
+
+
+        ElementosNavLat(icono = Icons.Default.Email, nombre = "Email")
+        ElementosNavLat(icono = Icons.Default.AccountCircle, nombre = "Mi cuenta")
+        Text(
+            text = "Powered by Meº",
+            Modifier.padding(start = 130.dp, top =30.dp),
+            color = Color.DarkGray,
+            fontFamily = FontFamily.Default,
+            fontStyle = FontStyle.Italic,
+            textAlign = TextAlign.Right
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+
+    }
+}
+
+@Composable
+fun Anuncio(
+    foto: Int,
+    icono: Int,
+    titulo: String,
+    texto: String,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .background(color = Color.LightGray, shape = RoundedCornerShape(9))
+            .padding(10.dp)
+            .height(100.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp)
+        ) {
+            Image(
+                painter = painterResource(id = foto),
+                contentDescription = null,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(9.dp))
+                    .align(Alignment.CenterVertically),
+                alignment = Alignment.Center
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Column(
+                verticalArrangement = Arrangement.Top,
+                modifier = Modifier
+                    .padding(3.dp)
+                    .align(Alignment.CenterVertically)
+            ) {
+                Text(
+                    text = titulo,
+                    style = MaterialTheme.typography.titleMedium,
+                    fontFamily = cooperFontFamily
+                )
+                Text(
+                    text = texto,
+                    style = MaterialTheme.typography.bodySmall,
+                    modifier = Modifier.verticalScroll(rememberScrollState())
+                )
+            }
+        }
+        Icon(
+            painter = painterResource(id = icono),
+            contentDescription = null,
+            modifier = Modifier.align(Alignment.BottomEnd)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MuestraAnuncio() {
+    Anuncio(
+        foto = R.drawable.googlemaps ,
+        icono = R.drawable.baseline_favorite_border_24,
+        titulo = "TITULO DEL ANUNCIO",
+        texto = "Aquí va un texto de prueba. Dispone de scroll, por lo que puede contener varias lineas mas de las que se muestran." +
+                "Sin embargo, solo ira una pequeña descripcion y el resto de detalles se verán al abrir el anuncio",
+        modifier = Modifier.fillMaxWidth()
+    )
+    Spacer(modifier = Modifier.height(5.dp))
+}
+
+@Composable
+fun ElementosNavLat(
+    icono : ImageVector,
+    nombre: String
+){
+    Row (
+        modifier = Modifier
+            .padding(4.dp)
+    ){
+        Icon(
+            imageVector = icono,
+            contentDescription = null,
+            modifier = Modifier
+                .size(24.dp)
+                .padding(start = 6.dp),
+            tint = MaterialTheme.colorScheme.primary
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = nombre,
+            style = MaterialTheme.typography.titleLarge,
+            fontFamily = cooperFontFamily
+        )
+    }
+}
+
+val cooperFontFamily = FontFamily(
+    Font(R.font.cooper, FontWeight.Bold))
